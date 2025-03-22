bc93d1db80820851daf2998f3368c2a1f8d1dce4
Aidan Bell <aidanrbell03@gmail.com>
2025-03-22 08:41:28 -0700

    Main Hub mostly completed

---
+++ Assets/Hub/Main Hub.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainHub : MonoBehaviour
+{
+    public float loadSceneWaitTime = 5f;
+    
+    bool changingScene = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void changeScene(int sceneIndex)
+    {
+        if (!changingScene)
+        {
+            changingScene = true;
+            StartCoroutine(loadScene(sceneIndex));
+        }
+    }
+
+    IEnumerator loadScene(int index)
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(index);
+    }
+
+
+}
--- Assets/Hub/Pin.cs
+++ Assets/Hub/Pin.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Pin : MonoBehaviour
 {
@@ -7,37 +10,68 @@ public class Pin : MonoBehaviour
     public string activityName;
     [SerializeField] private TextMeshProUGUI minigameText;
     [SerializeField] private GameObject canvas;
+    [SerializeField] private MainHub mainHub;
+
+    [SerializeField] private XRGrabInteractable xrGrabbin;
+
+    private Rigidbody body;
 
     const int ON_MAP = 0, HOVERING = 1, GRABBED = 2, PICKED = 3; // states of the pin
 
     private int state;
 
+    float focusOnTime = -1;
+
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("mmmmm");
+
         state = ON_MAP;
         minigameText.text = activityName;
         canvas.SetActive(false);
 
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        xrGrabbin.selectEntered.AddListener(grabbed);
+    }
+
+    private void OnDisable()
+    {
+        xrGrabbin.selectEntered.RemoveListener(grabbed);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (canvas.activeSelf && focusOnTime > 0)
+            focusOnTime -= Time.deltaTime;
+        if (focusOnTime <= 0 && !canvas.activeSelf)
+            canvas.SetActive(false);
+            
 
 
     }
 
     public void hoveringOver()
     {
+        focusOnTime = 5;
         canvas.SetActive(true);
-        // have some timer system possibly
     }
 
-    public void grabbed()
+    public void hoveringEnded()
     {
         canvas.SetActive(false);
-        // contact Hub Manager
     }
+    public void grabbed(SelectEnterEventArgs args)
+    {
+        canvas.SetActive(false);
+        body.useGravity = true;
+
+        mainHub.changeScene(sceneIndex);
+    }
+
 }
