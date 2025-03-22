59c58d7ccc68ab9438fcaa1f93612aaf05a77cc6
Aidan Bell <aidanrbell03@gmail.com>
2025-03-22 18:20:12 -0400

    Main Hub - Scene Map Finished

--- Assets/Hub/Main Hub.cs
+++ Assets/Hub/Main Hub.cs
@@ -31,7 +31,7 @@ public class MainHub : MonoBehaviour
 
     IEnumerator loadScene(int index)
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(loadSceneWaitTime);
         SceneManager.LoadScene(index);
     }
 
