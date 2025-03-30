756993bd8cc20eb44dbcb49907810652cfa7bc3e
blockheron <64575353+blockheron@users.noreply.github.com>
2025-03-30 19:13:58 -0400

    Shark spawns over water

--- Assets/Scripts/Outro/SharkSpawner.cs
+++ Assets/Scripts/Outro/SharkSpawner.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 // spawn an amount of sharks on the map randomly
 public class SharkSpawner : MonoBehaviour {
     
-    public GameObject mapShark;
+    [SerializeField] private GameObject mapShark;
+    [SerializeField] private GameObject[] spawnAreas;
+    public int taggedSharks;
 
-    // Update is called once per frame
     void Start() {
-        for (int i = 0; i < 10; i++) {
-            Vector3 randomSpawnPos = new Vector3(483.4f, Random.Range(22.0f, 21.0f), Random.Range(364.8f, 365.6f));
+        for (int i = 0; i < taggedSharks; i++) {
+
+            // select random spawn area
+            int randomSpawnArea = Random.Range(0, spawnAreas.Length);
+            Bounds spawnArea = spawnAreas[randomSpawnArea].GetComponent<Renderer>().bounds;
+
+            // spawn shark 483.4f
+            Vector3 randomSpawnPos = new Vector3(spawnArea.max.x, Random.Range(spawnArea.min.y, spawnArea.max.y), Random.Range(spawnArea.min.z, spawnArea.max.z));
             Instantiate(mapShark, randomSpawnPos, Quaternion.identity).transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
         }
     }
